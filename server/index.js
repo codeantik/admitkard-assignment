7f71b9c9cf65fa135d15727ac976acafa975b589
Ankit Singh <ankit.sin099@gmail.com>
2022-06-03 16:37:25 +0530

    added origin

--- server/index.js
+++ server/index.js
@@ -19,7 +19,9 @@ mongoose
 
 // middleware
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
